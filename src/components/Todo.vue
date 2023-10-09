dc1f3d248d9d73fd60969d6162a307100601d1b6
Diego Fornalha <diegofornalha@gmail.com>
2023-10-09 18:38:25 -0300

    Update Todo.vue

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,9 +3,8 @@
     class="container h-screen max-h-screen px-3 max-w-xl mx-auto flex flex-col"
   >
     <div class="my-auto p-16 rounded-lg text-center">
-      <div class="font-bold text-3xl md:text-5xl lg:text-6xl">
+      <div class="text-4xl font-bold mb-8">
         📝 <br />
-        &nbsp; toTooooDoooos
       </div>
 
       <form @submit="handleAddTodo">
@@ -13,7 +12,7 @@
           type="text"
           v-model="content"
           class="w-full my-8 px-6 py-4 text-xl rounded-lg border-0 focus:ring-2 focus:ring-gray-800 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl shadow-md"
-          placeholder="🤔   What to do today?"
+          placeholder="🚀 Foco do dia "
         />
       </form>
 
@@ -28,7 +27,7 @@
       @click="handleLogout"
       class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
     >
-      Logout 👋
+      Sair 👋
     </button>
   </section>
 </template>
