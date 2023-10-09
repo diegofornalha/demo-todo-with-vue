5ce3bf2b2d5cfcc4fb22b54ed2d9fa22c2b6483a
Diego Fornalha <diegofornalha@gmail.com>
2023-10-09 19:46:43 -0300

    blocto componente

---
+++ src/components/BloctoConnect.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <button @click="connectToBlocto">Conectar à Blocto</button>
+    </div>
+  </template>
+  
+  <script>
+  import Blocto from '@blocto/sdk';
+  
+  export default {
+    name: 'BloctoConnect',
+    data() {
+      return {
+        blocto: null
+      };
+    },
+    created() {
+      this.blocto = new Blocto({
+        env: 'testnet',
+        defaultChain: 'flow'
+      });
+    },
+    methods: {
+      async connectToBlocto() {
+        try {
+          await this.blocto.connect();
+          console.log('Conectado à Blocto!');
+        } catch (error) {
+          console.error('Erro ao conectar à Blocto:', error);
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para este componente, se necessário. */
+  </style>
+  
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 import store from './store'
+import Blocto from '@blocto/sdk';
+
+const blocto = new Blocto({
+    env: 'testnet', // ou 'mainnet' dependendo do ambiente
+    defaultChain: 'flow'
+  });
 
 
 createApp(App).use(router).use(store).mount('#app')
